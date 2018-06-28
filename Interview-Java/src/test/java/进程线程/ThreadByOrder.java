66550a8733c5c6202194718f116bfbd5499ae372
Enjoy <jun0415@foxmail.com>
2018-06-28 16:18:27 +0800

    线程

---
+++ Interview-Java/src/test/java/进程线程/ThreadByOrder.java
@@ -0,0 +1,52 @@
+package 进程线程;
+
+/*
+ * @desc
+ *  有三个线程T1 T2 T3，如何保证他们按顺序执行
+ *  在T2的run中，调用t1.join，让t1执行完成后再让T2执行
+ *  在T3的run中，调用t2.join，让t2执行完成后再让T3执行
+ * @author wjl
+ * @date 2018/6/28 0028
+ */
+public class ThreadByOrder {
+    static Thread T1 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("T1 RUN");
+        }
+    });
+
+    static Thread T2 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                T1.join();
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("T2 RUN");
+        }
+    });
+
+    static Thread T3 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                T2.join();
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("T3 RUN");
+        }
+    });
+
+    public static void main(String[] args) {
+        T1.start();
+        T2.start();
+        T3.start();
+    }
+}
---
+++ Interview-Java/src/test/java/进程线程/ThreadT.java
@@ -0,0 +1,30 @@
+package 进程线程;
+
+/*
+ * @desc 基础知识
+ * @author wjl
+ * @date 2018/6/28 0028
+ */
+public class ThreadT {
+    /* Thread类构造方法：
+    1.ThreadT（）
+    2.ThreadT（String name）
+    3.ThreadT（Runable r）
+    4.ThreadT（Runable r, String name）
+    */
+
+    /* thread类常用方法：
+    start(); // 启动线程
+    getId(); // 获得线程ID
+    getName(); // 获得线程名字
+    getPriority(); // 获得优先权
+    isAlive(); // 判断线程是否活动
+    isDaemon(); // 判断是否守护线程
+    getState(); // 获得线程状态
+    sleep(long mill);// 休眠线程
+    join(); // 等待线程结束
+    yield(); // 放弃cpu使用权利
+    interrupt(); // 中断线程
+    currentThread(); // 获得正在执行的线程对象
+    */
+}
