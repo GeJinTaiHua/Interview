4fc7c9b3240c6ad25f5dcf23e4609a2b24dcde77
Enjoy <jun0415@foxmail.com>
2018-07-27 17:28:49 +0800

    枚举单例

---
+++ Interview-Java/src/main/java/www/wjl/com/Interview/entity/SingleE.java
@@ -0,0 +1,9 @@
+package www.wjl.com.Interview.entity;
+
+/*
+ * @desc 枚举单例的示例
+ * @author wjl
+ * @date 2018/7/27 0027
+ */
+public class SingleE {
+}
--- Interview-Java/src/test/java/Pattern/Singleton/SingletonEnum.java
+++ Interview-Java/src/test/java/Pattern/Singleton/SingletonEnum.java
@@ -1,13 +1,22 @@
 package Pattern.Singleton;
 
+import www.wjl.com.Interview.entity.SingleE;
+
 /*
  * @desc 枚举单例
+ *  SingletonEnum.instance.getInstance()
  * @author wjl
  * @date 2018/7/27 0027
  */
 public enum SingletonEnum {
     instance;
+    private SingleE ins;
 
-    public void whateverMethod() {
+    private SingletonEnum() {
+        ins = new SingleE();
+    }
+
+    public SingleE getInstance() {
+        return ins;
     }
 }
