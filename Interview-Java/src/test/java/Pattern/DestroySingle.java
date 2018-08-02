4ce37b548935590b85d844e2a93a4de7a82e668c
Enjoy <jun0415@foxmail.com>
2018-08-02 16:45:11 +0800

    利用反序列化破坏

--- Interview-Java/src/test/java/Pattern/DestroySingle.java
+++ Interview-Java/src/test/java/Pattern/DestroySingle.java
@@ -4,8 +4,11 @@ import Pattern.Singleton.SingletonHungry;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
-
 
 /*
  * @desc 破坏单例
@@ -16,6 +19,7 @@ import java.lang.reflect.Constructor;
 public class DestroySingle {
     /*
      * @desc 利用反射破坏
+     * 通过反射把单例的构造函数设置为可访问，然后去生成一个新的对象。
      * @author wjl
      * @date 2018/8/2 0002
      */
@@ -31,5 +35,27 @@ public class DestroySingle {
         cons.setAccessible(true);
         SingletonHungry C = (SingletonHungry) cons.newInstance(null);
         System.out.println("A=C:" + (A == C)); //A=C:false
+    }
+
+    /*
+     * @desc 利用反序列化破坏
+     * @author wjl
+     * @date 2018/8/2 0002
+     */
+    @Test
+    public void testSerialize() throws Exception {
+        SingletonHungry A = SingletonHungry.getInstance();
+        FileOutputStream fos = new FileOutputStream("test.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(A);
+        oos.flush();
+        oos.close();
+
+        SingletonHungry B = null;
+        FileInputStream fls = new FileInputStream("test.txt");
+        ObjectInputStream ols = new ObjectInputStream(fls);
+        B = (SingletonHungry) ols.readObject();
+
+        System.out.println("A=B:" + (A == B)); //A=B:false
     }
 }
--- Interview-Java/src/test/java/Pattern/Singleton/SingletonHungry.java
+++ Interview-Java/src/test/java/Pattern/Singleton/SingletonHungry.java
@@ -1,11 +1,13 @@
 package Pattern.Singleton;
 
+import java.io.Serializable;
+
 /*
  * @desc 饿汉模式
  * @author wjl
  * @date 2018/7/27 0027
  */
-public class SingletonHungry {
+public class SingletonHungry implements Serializable {
     private static SingletonHungry instance = null;
 
     static {
