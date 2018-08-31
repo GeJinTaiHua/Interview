d5970e82ce7aa3d3dfc42e9b7a61c340bddca3ae
Enjoy <jun0415@foxmail.com>
2018-08-31 14:30:58 +0800

    函数式接口

---
+++ Interview-Java/src/test/java/BaseData/FunctionInterfaceDemo.java
@@ -0,0 +1,21 @@
+package BaseData;
+
+/*
+ * @desc 函数式接口
+ * @author wjl
+ * @date 2018/8/31 0031
+ */
+public class FunctionInterfaceDemo {
+    @FunctionalInterface
+    interface Predicate<T> {
+        boolean test(T t);
+    }
+
+    private static boolean doPredicate(int age, Predicate<Integer> predicate) {
+        return predicate.test(age);
+    }
+
+    public static void main(String args) {
+        boolean is = doPredicate(20, x -> x >= 18);
+    }
+}
---
+++ Interview-Java/src/test/java/BaseData/Lambda.java
@@ -0,0 +1,18 @@
+package BaseData;
+
+import org.junit.Test;
+
+/*
+ * @desc Lambda表达式
+ * @author wjl
+ * @date 2018/8/31 0031
+ */
+public class Lambda {
+    //int sum = (x, y) -> x + y;
+
+    @Test
+    public void test() {
+
+
+    }
+}
