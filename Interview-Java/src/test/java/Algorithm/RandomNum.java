2ffc3e19b2dd93366278a889a700b40dd14d0425
Enjoy <jun0415@foxmail.com>
2018-09-13 15:49:04 +0800

    1

--- Interview-Java/src/test/java/Algorithm/RandomNum.java
+++ Interview-Java/src/test/java/Algorithm/RandomNum.java
@@ -18,8 +18,44 @@ public class RandomNum {
      * @date 2018/9/13 0013
      */
     @Test
-    public void Creat100Num() {
+    public void get100Num() {
         Random rand = new Random();
         int result = rand.nextInt(100) + 1;
     }
+
+    /*
+     * @desc  MIN 和 MAX 范围内的随机数
+     * @author wjl
+     * @date 2018/9/13 0013
+     */
+    public int getRandNum(int min, int max) {
+        int randNum = (int) (Math.random() * ((max - min) + 1)) + min;
+        return randNum;
+    }
+
+    /*
+     * @desc 生成 length长度 的随机数字和字母
+     * @author wjl
+     * @date 2018/9/13 0013
+     */
+    public String getStringRandom(int length) {
+        String result = "";
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            // 字母还是数字
+            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";
+
+            if ("char".equals(charOrNum)) {
+                // 大写字母还是小写字母
+                int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;
+                result += (char) (random.nextInt(26) + temp);
+            } else if ("num".equals(charOrNum)) {
+                result += String.valueOf(random.nextInt(10));
+            }
+        }
+
+        return result;
+    }
+
 }
