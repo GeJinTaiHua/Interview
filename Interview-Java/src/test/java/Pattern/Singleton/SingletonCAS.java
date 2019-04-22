0da9cf7471aee48df75966ce4838c7cc66a90510
小龙 <jun0415@foxmail.com>
2019-04-22 10:50:26 +0800

    乐观锁技术单例

--- Interview-Java/src/test/java/Pattern/Singleton/SingletonCAS.java
+++ Interview-Java/src/test/java/Pattern/Singleton/SingletonCAS.java
@@ -2,21 +2,24 @@ package Pattern.Singleton;
 
 /*
  * @desc 乐观锁技术
+ * 好处：不需要使用传统的锁机制来保证线程安全,CAS是一种基于忙等待的算法,依赖底层硬件的实现,相对于锁它没有线程切换和阻塞的额外消耗,可以支持较大的并行度。
+ * 缺点：如果忙等待一直执行不成功(一直在死循环中),会对CPU造成较大的执行开销；
+ *      如果N个线程同时执行到singleton = new Singleton();的时候，会有大量对象创建，很可能导致内存溢出。
  * @date 2019/4/22 0024
  */
 public class SingletonCAS {
-    private static final AtomicReference<Singleton> INSTANCE = new AtomicReference<Singleton>();
+    private static final AtomicReference<SingletonCAS> INSTANCE = new AtomicReference<SingletonCAS>();
 
-    private Singleton() {}
+    private SingletonCAS() {}
 
-    public static Singleton getInstance() {
+    public static SingletonCAS getInstance() {
         for (;;) {
-            Singleton singleton = INSTANCE.get();
+            SingletonCAS singleton = INSTANCE.get();
             if (null != singleton) {
                 return singleton;
             }
 
-            singleton = new Singleton();
+            singleton = new SingletonCAS();
             if (INSTANCE.compareAndSet(null, singleton)) {
                 return singleton;
             }
