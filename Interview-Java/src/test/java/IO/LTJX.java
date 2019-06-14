caa39b766938975ef1227e17b25798330ec13f6a
xiaolong <wangjunlong@78dk.com>
2019-06-14 15:06:19 +0800

    .

--- Interview-Java/src/test/java/IO/LTJX.java
+++ Interview-Java/src/test/java/IO/LTJX.java
@@ -37,7 +37,7 @@ public class LTJX {
     }
 
     /**
-     * 读文件，对文件内容排序，输出另外文件
+     * 读文件，对文件内容排序，输出另外文件1
      */
     @Test
     public void test2() throws Exception {
@@ -59,6 +59,36 @@ public class LTJX {
 
             Arrays.sort(newStr);
             writer.write(newStr);
+
+            reader.close();
+            writer.close();
+        }
+    }
+
+    /**
+     * 读文件，对文件内容排序，输出另外文件2
+     */
+    @Test
+    public void test3() throws Exception {
+        File oldFile = new File("Pic" + File.separator + "test3old.txt");
+        File newFile = new File("Pic" + File.separator + "test3new.txt");
+
+        if (oldFile.exists() && oldFile.isFile()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(oldFile)));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(newFile)));
+
+            StringBuilder sb = new StringBuilder();
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                sb.append(temp);
+            }
+
+            String oldStr = sb.toString();
+            char[] newChar = oldStr.toCharArray();
+            Arrays.sort(newChar);
+
+            writer.write(newChar);
+            writer.flush();
 
             reader.close();
             writer.close();
