03c28c27f42a1109b750ec1f610a624889be4b6c
jun04 <jun04>
2021-05-07 18:41:23 +0800

    删除本地class再调试

--- Interview-Java/src/test/java/ClassLoad/MyNetworkClassLoader.java
+++ Interview-Java/src/test/java/ClassLoad/MyNetworkClassLoader.java
@@ -18,7 +18,7 @@ public class MyNetworkClassLoader extends ClassLoader {
     }
 
     @Override
-    public Class findClass(String name) {
+    public Class<?> findClass(String name) {
         byte[] b = loadClassData(name);
         return defineClass(name, b, 0, b.length);
     }
